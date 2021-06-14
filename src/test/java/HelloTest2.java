c29778fac56071863aa7144ab5ce96463963ec06
Zeynep-bak <zeynepsimsek81@mail.com>
2021-06-14 10:28:34 +0200

    work 1 done

--- src/test/java/HelloTest2.java
+++ src/test/java/HelloTest2.java
@@ -2,6 +2,10 @@ public class HelloTest2 {
     public static void main(String[] args) {
 
         System.out.println("Hello Test2");
-        System.out.println("About the new project on GitHub");
+
+        //work1
+
+
+
     }
 }
